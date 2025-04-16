baa232801fcc16e60b1d13f44a0fbbd189e881ea
Tushar Saini <s2sharpit@gmail.com>
2025-04-16 17:48:50 +0530

    283: Add Firebase authentication and chat functionality
    - Created firebase.json for Firebase configuration.
    - Generated firebase_options.dart for platform-specific Firebase options.
    - Implemented AuthScreen for user authentication with email and password.
    - Developed ChatScreen to display a simple chat interface and sign-out functionality.
    - Added SplashScreen as an initial loading screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'firebase_options.dart';
+import 'package:chat_app/screens/chat.dart';
+import 'package:chat_app/screens/splash.dart';
+import 'package:chat_app/screens/auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 63, 17, 177),
         ),
       ),
+      home: StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const SplashScreen();
+        }
+        if (snapshot.hasData) {
+          return const ChatScreen();
+        }
+        return const AuthScreen();
+      }),
     );
   }
 }
